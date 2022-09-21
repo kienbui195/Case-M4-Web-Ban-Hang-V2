13415b35eb01210dd64915c354674c4b210bd0d5
Kay Bi <kaybi9x@gmail.com>
2022-09-21 17:04:04 +0700

    update

--- src/controllers/controller.ts
+++ src/controllers/controller.ts
@@ -4,6 +4,10 @@ class Controller {
     showHomePage(req: any, res: any) {
         res.render('home');
     }
+
+    showAbout(req: any, res: any) {
+        res.render('about');
+    }
 }
 
 export default Controller;
--- src/routers/web.router.ts
+++ src/routers/web.router.ts
@@ -6,8 +6,11 @@ const router = express.Router();
 const controller = new Controller();
 
 router.get('/', (req, res) => {
-    console.log(req.originalUrl)
     controller.showHomePage(req, res);
+})
+
+router.get('/about', (req, res) => {
+    controller.showAbout(req, res);
 })
 
 export default router;
