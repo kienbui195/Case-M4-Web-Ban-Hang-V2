604ea9565d437a45e2d519f388f4f47a1ce3cd6c
Kay Bi <kaybi9x@gmail.com>
2022-09-22 13:41:36 +0700

    update

--- index.ts
+++ index.ts
@@ -1,14 +1,32 @@
 import express from "express";
 import router from "./src/routers/web.router";
+import mongoose from "mongoose";
+import session from "express-session";
+import flash from "connect-flash";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
 
 const app = express();
 const port = 8000;
+const DB_URL = 'mongodb://localhost:27017/caseM4';
 
 app.set('view engine', 'ejs');
 app.set('views', './src/views');
 app.use(express.static('src/public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cookieParser('secret'));
+app.use(session({
+    secret: "secret",
+    cookie: { maxAge: 60000 },
+}));
 
+app.use(flash());
 app.use('/', router);
+
+mongoose.connect(DB_URL)
+    .then(() => console.log('DB connected'))
+    .catch(err => console.log(err.message));
 
 app.listen(port, () => {
     console.log(`running at http://localhost:${port}`);
--- src/controllers/controller.ts
+++ src/controllers/controller.ts
@@ -1,3 +1,6 @@
+import { UserModel } from "../schemas/userLogin.model";
+import * as popup from "node-popup";
+import {alert} from 'node-popup';
 
 class Controller {
 
@@ -9,8 +12,27 @@ class Controller {
         res.render('login');
     }
 
-    showDashboardPage(req:any, res:any) {
+    showDashboardPage(req: any, res: any) {
         res.render('dashboard');
+    }
+
+    async getDataRegister(req: any, res: any) {
+        const user = await UserModel.findOne({ email: req.body.emailRegister });
+        if (!user) {
+            const data = req.body;
+            const newUser = {
+                name: data.nameRegister,
+                email: data.emailRegister,
+                password: data.passwordRegister
+            }
+            await UserModel.create(newUser);
+            res.render('login', {messageSuccess: 'Register Success!'})
+        }
+    }
+
+    logout(req: any, res: any) {
+        req.flash('message', 'You are now logged out.');
+        res.redirect('/login');
     }
 }
 
